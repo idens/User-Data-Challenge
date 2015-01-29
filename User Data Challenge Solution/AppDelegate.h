f6e307dff6b98e74ee33d4aa1afa7ab2e5af0a7d
iDens <denis.roks@gmail.com>
2015-01-29 15:13:16 +0300

    Initial Commit

---
+++ User Data Challenge Solution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  User Data Challenge Solution
+//
+//  Created by Admin on 29.01.15.
+//  Copyright (c) 2015 Denis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ User Data Challenge Solution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  User Data Challenge Solution
+//
+//  Created by Admin on 29.01.15.
+//  Copyright (c) 2015 Denis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
